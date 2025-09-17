857d5c6e64a214bd30ed0fa58153a183d986a058
kms6031 <dr701050@kbu.ac.kr>
2025-09-17 21:40:53 +0900

    init

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose";
+import filesRouter from './routes/files.js'
 
 const PORT = process.env.PORT
 await mongoose.connect(process.env.MONGO_URI)
@@ -13,6 +14,7 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use('/api/files',filesRouter)
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
--- backend/routes/files.js
+++ backend/routes/files.js
@@ -4,29 +4,136 @@ import FileItem from '../models/FileItem.js'
 import {
     presignGet,
     presignPut,
-    deleteObject} from '../src/s3.js'
+    deleteObject
+} from '../src/s3.js'
 
-const router =Router()
+const router = Router()
 
 
-router.post('/presign',async(req,res)=>{
+router.post('/presign', async (req, res) => {
     try {
-        const {filename, contentType}=req.body
-        if(!filename || !contentType){
-            return res.status(400).json({message:"filename/contentType은 필수 입니다."})
+        const { filename, contentType } = req.body
+        if (!filename || !contentType) {
+            return res.status(400).json({ message: "filename/contentType은 필수 입니다." })
         }
 
-        const key =`uploads/${Date.now()}-${nanoid(6)}-${filename}`
+        const key = `uploads/${Date.now()}-${nanoid(6)}-${filename}`
 
-        const url = await presignPut(key,contentType)
+        const url = await presignPut(key, contentType)
 
-        res.json({url,key})
+        res.json({ url, key })
 
     } catch (error) {
         console.error(error)
-        res.status(500).json({error:"프리사인드 url 생성 실패"})
+        res.status(500).json({ error: "프리사인드 url 생성 실패" })
     }
 })
+
+router.post('/', async (req, res) => {
+    try {
+
+        const {
+            key,
+            originalName,
+            contentType,
+            size,
+            title = "",
+            description = ""
+        } = req.body
+
+        const doc = await FileItem.create({
+            key,
+            originalName,
+            contentType,
+            size,
+            title,
+            description
+        })
+        res.status(201).json({ message: "S3 메타데이터 저장 완료", doc })
+
+    } catch (error) {
+        console.error('메타데이터 저장 에러', error)
+        res.status(500).json({ error: "S3 메타데이터 저장 실패" })
+    }
+})
+router.get('/', async (req, res) => {
+    try {
+
+        const items = await FileItem.find().sort({ createdAt: -1 }).lean()
+
+        const out = await Promise.all(
+            items.map(async (it) => ({
+                ...it,
+                url: await presignGet(it.key, 300)
+            }))
+        )
+
+        res.status(201).json({ message: "S3 메타데이터 가져오기", out })
+
+    } catch (error) {
+        console.error('메타데이터 저장 에러', error)
+        res.status(500).json({ error: "S3 메타데이터 저장 실패" })
+    }
+})
+router.get('/:id', async (req, res) => {
+    try {
+
+        const it = await FileItem.findById(req.params.id).lean()
+
+        if (!it) return res.sendStatus(404)
+
+        it.url = await presignGet(it.key, 300)
+
+        res.status(201).json({ message: "S3 메타데이터 단건 가져오기", it })
+
+    } catch (error) {
+        console.error('메타데이터 저장 에러', error)
+        res.status(500).json({ error: "S3 메타데이터 저장 실패" })
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+
+        const { title, description } = req.body
+        const it = await FileItem.findByIdAndUpdate(
+            req.params.id,
+            { title, description },
+            { new: true }
+        )
+
+        if (!it) return res.sendStatus(404)
+
+
+
+        res.status(201).json({ message: "S3 메타데이터 수정하기성공", it })
+
+    } catch (error) {
+        console.error('메타데이터 저장 에러', error)
+        res.status(500).json({ error: "S3 메타데이터 저장 실패" })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+
+        const it = await FileItem.findByIdAndDelete(req.params.id)
+
+        if (!it) return res.sendStatus(404)
+
+            await deleteObject(it.key)
+            await it.deleteOne()
+
+        res.status(201).json({ message: "파일 삭제 완료",id: req.params.id })
+
+    } catch (error) {
+        console.error('파일 삭제 에러', error)
+        res.status(500).json({ error: "S3 파일 삭제 실패" })
+    }
+})
+
+
 
 
 export default router
